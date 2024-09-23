bcce5d4221c8006c507da57c66893dd7b972a06b
calvincetom <calvincetom@outlook.com>
2024-09-23 11:18:21 +0300

    update template to include direct link to home

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
+"""Rest API modules"""
 from rest_framework.routers import DefaultRouter
-from orders.views import CustomerViewSet, OrderViewSet
 from django.urls import path, include
+from orders.views import CustomerViewSet, OrderViewSet
 
 # bind routes
 router = DefaultRouter()
@@ -10,4 +11,4 @@ router.register(r'orders', OrderViewSet)
 urlpatterns = [
     # OIDC URLs
     path('oidc/', include('mozilla_django_oidc.urls')),
-] + router.urls 
+] + router.urls
