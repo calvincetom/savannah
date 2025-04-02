e891c321e82652606e8799dac45991a7b4209581
calvincetom <calvincetom@outlook.com>
2025-04-02 10:51:59 +0300

    add order migration command in docker file

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,24 +1,30 @@
 """Modules to map model objects"""
+
 from rest_framework import serializers
 from .models import Customer, Order
 
+
 class CustomerSerializer(serializers.ModelSerializer):
     """Serializer for Customer objects"""
+
     class Meta:
         """Meta class for Customer Model"""
+
         model = Customer
-        fields = ['id', 'name','phone_number', 'code']
+        fields = ["id", "name", "phone_number", "code"]
+
 
 class OrderSerializer(serializers.ModelSerializer):
     """Serializer for Customer objects"""
 
     class Meta:
         """Meta class for Customer Model"""
+
         model = Order
-        fields = ['id', 'customer', 'item', 'amount', 'time']
+        fields = ["id", "customer", "item", "amount", "time"]
 
     def validate_amount(self, value):
         """Validate the amount value entered"""
         if value < 0:
-            raise serializers.ValidationError('Amount entered cannot be negative.')
+            raise serializers.ValidationError("Amount entered cannot be negative.")
         return value
